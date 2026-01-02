aa2e2bd9cbf6e6a0d81d27e73035e4456ac59b0a
range79 <darkrange006@gmail.com>
2026-01-02 08:41:31 +0400

    bunnyUploader class added

--- lib/src/main/java/com/range/upload/BunnyUploader.java
+++ lib/src/main/java/com/range/upload/BunnyUploader.java
@@ -2,8 +2,17 @@ package com.range.upload;
 
 import com.range.dto.PutObjectRequest;
 import com.range.dto.PutObjectResponse;
+import com.range.enums.Region;
 
 public interface BunnyUploader {
-    PutObjectResponse uploadFileBunny(PutObjectRequest putObjectRequest);
 
+    /**
+     * Uploads a file to BunnyCDN.
+     *
+     * @param putObjectRequest The request containing all required information:
+     *                         bucket name, key (file path), content type, metadata, and file data as InputStream.
+     * @return {@link PutObjectResponse} containing the bucket, key, and public URL of the uploaded file.
+     * @throws RuntimeException if the upload fails due to network, authentication, or other errors.
+     */
+    PutObjectResponse uploadFileBunny(PutObjectRequest putObjectRequest);
 }
--- lib/src/main/java/com/range/upload/impl/BunnyUploaderImpl.java
+++ lib/src/main/java/com/range/upload/impl/BunnyUploaderImpl.java
@@ -1,12 +1,19 @@
 package com.range.upload.impl;
 
 import com.range.config.BunnyNetClient;
+import com.range.dto.PutObjectRequest;
+import com.range.dto.PutObjectResponse;
+import com.range.upload.BunnyUploader;
 
-public class BunnyUploaderImpl {
+public class BunnyUploaderImpl implements BunnyUploader {
     private final BunnyNetClient bunnyNetClient;
     public BunnyUploaderImpl(BunnyNetClient bunnyNetClient){
         this.bunnyNetClient = bunnyNetClient;
     }
 
 
+    @Override
+    public PutObjectResponse uploadFileBunny(PutObjectRequest putObjectRequest) {
+
+    }
 }
