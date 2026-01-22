fe1e5c4fe9e291237f3270c8aa68d1305a2af9d4
range79 <darkrange006@gmail.com>
2026-01-22 18:28:45 +0400

    download method implemented

--- lib/src/main/java/com/range/multi/MultiBunnyStorageImpl.java
+++ lib/src/main/java/com/range/multi/MultiBunnyStorageImpl.java
@@ -4,21 +4,25 @@ import com.range.common.dto.PutObjectRequest;
 import com.range.common.dto.PutObjectResponse;
 import com.range.common.enums.Region;
 import com.range.multi.config.MultiBunnyNetConfig;
+import com.range.multi.downloader.MultiBunnyDownloader;
 import com.range.multi.upload.MultiBunnyUploader;
 
 import java.io.InputStream;
 
 public class MultiBunnyStorageImpl implements MultiBunnyStorage {
     private final MultiBunnyUploader uploader;
+    private final MultiBunnyDownloader downloader;
 
     //private final MultiBunnyDelete delete;
-    public MultiBunnyStorageImpl(MultiBunnyNetConfig config){
-        this.uploader=MultiBunnyUploader.create(config);
-    //    this.delete=MultiBunnyDelete;
+    public MultiBunnyStorageImpl(MultiBunnyNetConfig config) {
+        this.uploader = MultiBunnyUploader.create(config);
+        this.downloader = MultiBunnyDownloader.create(config);
+        //    this.delete=MultiBunnyDelete;
     }
+
     public PutObjectResponse uploadFile(PutObjectRequest putObjectRequest, String storageZoneName,
-                                        Region storageRegion){
-        return  uploader.uploadFileBunny(putObjectRequest,storageZoneName,storageRegion);
+                                        Region storageRegion) {
+        return uploader.uploadFileBunny(putObjectRequest, storageZoneName, storageRegion);
     }
 
     @Override
@@ -28,7 +32,8 @@ public class MultiBunnyStorageImpl implements MultiBunnyStorage {
 
     @Override
     public InputStream downloadFile(String storageZoneName, String key, Region storageRegion) {
-        return null;
+
+        return downloader.download(storageZoneName,key,storageRegion) ;
     }
 
 }
