f22d1e2d210ec8c03e42aefef3548ba1579962db
range79 <darkrange006@gmail.com>
2026-03-03 07:20:45 +0400

    new thing testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.plugins.JavaPluginExtension
+
 plugins {
     id("com.palantir.git-version") version "3.0.0"
-    id("org.jreleaser") version "1.13.0"
     `maven-publish`
+    signing
     base
 }
 
 group = "io.github.range79"
 
-// -------- TAG BASED VERSION --------
+// -------- VERSION (TAG BASED) --------
 val gitVersion: groovy.lang.Closure<String> by extra
 val rawVersion = gitVersion()
 
@@ -18,74 +21,35 @@ version = if (rawVersion.matches(semverRegex)) {
     "0.0.0-SNAPSHOT"
 }
 
-// -------- SUBPROJECTS --------
+// -------- SUBPROJECT CONFIG --------
 subprojects {
+
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     group = rootProject.group
     version = rootProject.version
 
-    extensions.configure<org.gradle.api.plugins.JavaPluginExtension> {
+    // 🔥 Java extension doğru şekilde configure
+    extensions.configure<JavaPluginExtension> {
         withSourcesJar()
         withJavadocJar()
     }
 
-    extensions.configure<org.gradle.api.publish.PublishingExtension> {
+    publishing {
         publications {
-            create<org.gradle.api.publish.maven.MavenPublication>("mavenJava") {
+            create<MavenPublication>("mavenJava") {
                 from(components["java"])
-
-                pom {
-                    name.set(project.name)
-                    description.set("Unofficial Bunny.net Java SDK")
-                    url.set("https://github.com/range79/bunnynet-lib")
-
-                    licenses {
-                        license {
-                            name.set("Apache License 2.0")
-                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("range79")
-                            name.set("Azad Dadasov")
-                            email.set("darkrange6@gmail.com")
-                        }
-                    }
-
-                    scm {
-                        connection.set("scm:git:git://github.com/range79/bunnynet-lib.git")
-                        developerConnection.set("scm:git:ssh://github.com/range79/bunnynet-lib.git")
-                        url.set("https://github.com/range79/bunnynet-lib")
-                    }
-                }
             }
         }
     }
-}
-
-// -------- JRELEASER --------
-jreleaser {
 
     signing {
-        active.set(org.jreleaser.model.Active.ALWAYS)
-        armored.set(true)
-    }
-
-    deploy {
-        maven {
-            mavenCentral {
-                create("sonatype") {
-                    active.set(org.jreleaser.model.Active.ALWAYS)
-                    url.set("https://central.sonatype.com/api/v1/publisher")
-
-                    username.set(System.getenv("OSSRH_USERNAME"))
-                    password.set(System.getenv("OSSRH_PASSWORD"))
-                }
-            }
-        }
+        useInMemoryPgpKeys(
+            System.getenv("GPG_PRIVATE_KEY"),
+            System.getenv("GPG_PASSPHRASE")
+        )
+        sign(publishing.publications)
     }
 }
