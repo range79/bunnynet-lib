4a03608d3bc9dc03e2eda53cda30115546aec4ce
range79 <darkrange006@gmail.com>
2026-03-03 07:33:33 +0400

    it will work (maybe)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.plugins.JavaPluginExtension
+import org.jreleaser.model.Active
 
 plugins {
     id("com.palantir.git-version") version "3.0.0"
+    id("org.jreleaser") version "1.13.0"
     `maven-publish`
     signing
     base
@@ -10,20 +12,14 @@ plugins {
 
 group = "io.github.range79"
 
-// -------- VERSION (TAG BASED) --------
 val gitVersion: groovy.lang.Closure<String> by extra
 val rawVersion = gitVersion()
-
 val semverRegex = Regex("^v?\\d+\\.\\d+\\.\\d+(?:-[0-9A-Za-z.-]+)?$")
-version = if (rawVersion.matches(semverRegex)) {
-    rawVersion.removePrefix("v")
-} else {
-    "0.0.0-SNAPSHOT"
-}
+version = if (rawVersion.matches(semverRegex)) rawVersion.removePrefix("v") else "0.0.0-SNAPSHOT"
 
-// -------- SUBPROJECT CONFIG --------
+val stagingDir = layout.buildDirectory.dir("staging-deploy")
+
 subprojects {
-
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
     apply(plugin = "signing")
@@ -36,60 +32,55 @@ subprojects {
         withJavadocJar()
     }
 
-    publishing {
-
+    extensions.configure<org.gradle.api.publish.PublishingExtension> {
         publications {
             create<MavenPublication>("mavenJava") {
                 from(components["java"])
-
-                pom {
-                    name.set(project.name)
-                    description.set("Unofficial Bunny.net Java SDK")
-                    url.set("https://github.com/range79/bunnynet-lib")
-
-                    licenses {
-                        license {
-                            name.set("Apache License 2.0")
-                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("range79")
-                            name.set("Azad Dadasov")
-                            email.set("darkrange6@gmail.com")
-                        }
-                    }
-
-                    scm {
-                        connection.set("scm:git:git://github.com/range79/bunnynet-lib.git")
-                        developerConnection.set("scm:git:ssh://github.com/range79/bunnynet-lib.git")
-                        url.set("https://github.com/range79/bunnynet-lib")
-                    }
-                }
             }
         }
 
-        // 🔥 BURASI KRİTİK
         repositories {
             maven {
-                name = "central"
-                url = uri("https://central.sonatype.com/api/v1/publisher")
+                name = "staging"
+                url = uri(stagingDir.get().asFile.toURI())
+            }
+        }
+    }
 
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
+    extensions.configure<org.gradle.plugins.signing.SigningExtension> {
+        useInMemoryPgpKeys(
+            System.getenv("JRELEASER_GPG_SECRET_KEY") ?: System.getenv("GPG_PRIVATE_KEY"),
+            System.getenv("JRELEASER_GPG_PASSPHRASE") ?: System.getenv("GPG_PASSPHRASE")
+        )
+        sign(extensions.getByType<org.gradle.api.publish.PublishingExtension>().publications)
+    }
+}
+
+jreleaser {
+    signing {
+        active.set(Active.ALWAYS)
+        armored.set(true)
+
+    }
+
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active.set(Active.ALWAYS)
+                    url.set("https://central.sonatype.com/api/v1/publisher")
+                    username.set(System.getenv("OSSRH_USERNAME"))
+                    password.set(System.getenv("OSSRH_PASSWORD"))
+
+                    stagingRepositories.add(stagingDir.get().asFile.absolutePath)
                 }
             }
         }
     }
 
-    signing {
-        useInMemoryPgpKeys(
-            System.getenv("GPG_PRIVATE_KEY"),
-            System.getenv("GPG_PASSPHRASE")
-        )
-        sign(publishing.publications)
+    release {
+        github {
+            token.set(System.getenv("JRELEASER_GITHUB_TOKEN"))
+        }
     }
 }
